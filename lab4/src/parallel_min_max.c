aead075c20f5f155ed6935d8b93c9eae9f10dd6b
Simon-Struggler <sema.patyukov.84@mail.ru>
2025-11-14 20:39:58 +0000

    fix1task

--- lab4/src/parallel_min_max.c
+++ lab4/src/parallel_min_max.c
@@ -18,27 +18,42 @@
 #include "find_min_max.h"
 #include "utils.h"
 
+// Глобальные переменные для обработки сигналов
+volatile sig_atomic_t timeout_occurred = 0;
+pid_t *child_pids = NULL;
+int pnum_global = 0;
+
+// Обработчик сигнала SIGALRM
+void timeout_handler(int sig) {
+    timeout_occurred = 1;
+    printf("Timeout occurred! Sending SIGKILL to all child processes...\n");
+    
+    for (int i = 0; i < pnum_global; i++) {
+        if (child_pids[i] > 0) {
+            kill(child_pids[i], SIGKILL);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
   int seed = -1;
   int array_size = -1;
   int pnum = -1;
-  int timeout = 0; // 0 means no timeout
+  int timeout = 0; // 0 означает отсутствие таймаута
   bool with_files = false;
 
   while (true) {
     int current_optind = optind ? optind : 1;
 
-    static struct option options[] = {
-        {"seed", required_argument, 0, 0},
-        {"array_size", required_argument, 0, 0},
-        {"pnum", required_argument, 0, 0},
-        {"timeout", required_argument, 0, 0},
-        {"by_files", no_argument, 0, 'f'},
-        {0, 0, 0, 0}
-    };
+    static struct option options[] = {{"seed", required_argument, 0, 0},
+                                      {"array_size", required_argument, 0, 0},
+                                      {"pnum", required_argument, 0, 0},
+                                      {"by_files", no_argument, 0, 'f'},
+                                      {"timeout", required_argument, 0, 't'},
+                                      {0, 0, 0, 0}};
 
     int option_index = 0;
-    int c = getopt_long(argc, argv, "f", options, &option_index);
+    int c = getopt_long(argc, argv, "ft:", options, &option_index);
 
     if (c == -1) break;
 
@@ -67,13 +82,6 @@ int main(int argc, char **argv) {
             }
             break;
           case 3:
-            timeout = atoi(optarg);
-            if (timeout <= 0) {
-                printf("timeout must be a positive number\n");
-                return 1;
-            }
-            break;
-          case 4:
             with_files = true;
             break;
 
@@ -83,6 +91,13 @@ int main(int argc, char **argv) {
         break;
       case 'f':
         with_files = true;
+        break;
+      case 't':
+        timeout = atoi(optarg);
+        if (timeout <= 0) {
+            printf("timeout must be a positive number\n");
+            return 1;
+        }
         break;
 
       case '?':
@@ -99,12 +114,19 @@ int main(int argc, char **argv) {
   }
 
   if (seed == -1 || array_size == -1 || pnum == -1) {
-    printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" [--timeout \"num\"]\n",
+    printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" [--timeout \"seconds\"]\n",
            argv[0]);
     return 1;
   }
 
+  // Выделяем память для хранения PID дочерних процессов
+  child_pids = malloc(pnum * sizeof(pid_t));
+  pnum_global = pnum;
+
+  // Настраиваем обработчик сигнала SIGALRM если задан таймаут
   if (timeout > 0) {
+    signal(SIGALRM, timeout_handler);
+    alarm(timeout); // Устанавливаем таймер
     printf("Timeout set to %d seconds\n", timeout);
   }
 
@@ -129,7 +151,6 @@ int main(int argc, char **argv) {
   }
 
   int active_child_processes = 0;
-  pid_t child_pids[pnum]; // Сохраняем PID всех дочерних процессов
 
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
@@ -143,7 +164,7 @@ int main(int argc, char **argv) {
     if (child_pid >= 0) {
       // successful fork
       active_child_processes += 1;
-      child_pids[i] = child_pid; // Сохраняем PID дочернего процесса
+      child_pids[i] = child_pid; // сохраняем PID дочернего процесса
       
       if (child_pid == 0) {
         // child process
@@ -188,101 +209,90 @@ int main(int argc, char **argv) {
     }
   }
 
-  // Ожидание завершения дочерних процессов с таймаутом
-  if (timeout > 0) {
-    int time_elapsed = 0;
-    while (active_child_processes > 0 && time_elapsed < timeout) {
-      sleep(1); // Ждем 1 секунду
-      time_elapsed++;
-      
-      // Проверяем, не завершились ли какие-то процессы
-      pid_t finished_pid;
-      int status;
-      while ((finished_pid = waitpid(-1, &status, WNOHANG)) > 0) {
-        active_child_processes -= 1;
-        if (WIFEXITED(status)) {
-          printf("Child process %d exited normally\n", finished_pid);
-        } else if (WIFSIGNALED(status)) {
-          printf("Child process %d terminated by signal %d\n", finished_pid, WTERMSIG(status));
-        }
-      }
-    }
+  // Ожидание завершения дочерних процессов с возможностью таймаута
+  while (active_child_processes > 0) {
+    int status;
+    pid_t finished_pid;
     
-    // Если время вышло, а процессы еще остались - убиваем их
-    if (active_child_processes > 0) {
-      printf("Timeout reached! Sending SIGKILL to %d remaining child processes\n", active_child_processes);
-      for (int i = 0; i < pnum; i++) {
-        // Проверяем, жив ли еще процесс
-        if (kill(child_pids[i], 0) == 0) {
-          // Процесс еще жив - отправляем SIGKILL
-          if (kill(child_pids[i], SIGKILL) == 0) {
-            printf("Sent SIGKILL to child process %d\n", child_pids[i]);
-          } else {
-            perror("Failed to send SIGKILL");
-          }
-        }
-      }
-      
-      // Ждем завершения убитых процессов
-      while (active_child_processes > 0) {
-        wait(NULL);
-        active_child_processes -= 1;
-      }
-    }
-  } else {
-    // Без таймаута - обычное ожидание
-    while (active_child_processes > 0) {
-      wait(NULL);
+    // Неблокирующее ожидание с WNOHANG
+    finished_pid = waitpid(-1, &status, WNOHANG);
+    
+    if (finished_pid > 0) {
+      // Один из дочерних процессов завершился
       active_child_processes -= 1;
+      
+      if (WIFEXITED(status)) {
+        printf("Child process %d exited normally\n", finished_pid);
+      } else if (WIFSIGNALED(status)) {
+        printf("Child process %d killed by signal %d\n", finished_pid, WTERMSIG(status));
+      }
+    } else if (finished_pid == 0) {
+      // Дочерние процессы еще работают, проверяем таймаут
+      if (timeout_occurred) {
+        printf("Timeout! Some processes were terminated\n");
+        break;
+      }
+      // Небольшая пауза чтобы не нагружать CPU
+      //usleep(10000); // 10ms
+    } else {
+      // Ошибка waitpid
+      perror("waitpid failed");
+      break;
     }
+  }
+
+  // Отменяем таймер если он еще не сработал
+  if (timeout > 0 && !timeout_occurred) {
+    alarm(0);
   }
 
   struct MinMax min_max;
   min_max.min = INT_MAX;
   min_max.max = INT_MIN;
 
-  // Собираем результаты только от завершившихся процессов
+  int results_received = 0;
+  
   for (int i = 0; i < pnum; i++) {
     int min = INT_MAX;
     int max = INT_MIN;
-    bool result_available = false;
+    int result_available = 0;
 
     if (with_files) {
       // read from files
       FILE *file = fopen(filenames[i], "r");
       if (file != NULL) {
         if (fscanf(file, "%d %d", &min, &max) == 2) {
-          result_available = true;
+          result_available = 1;
         }
         fclose(file);
         // Удаляем временный файл
         remove(filenames[i]);
       }
     } else {
-      // read from pipes - используем неблокирующее чтение
+      // read from pipes
+      // Проверяем, есть ли данные для чтения
       fd_set readfds;
       struct timeval tv;
       
       FD_ZERO(&readfds);
       FD_SET(pipefd[i * 2], &readfds);
       tv.tv_sec = 0;
-      tv.tv_usec = 10000; // 10ms timeout
+      tv.tv_usec = 1000; // 1ms timeout
       
-      // Проверяем, есть ли данные для чтения
       if (select(pipefd[i * 2] + 1, &readfds, NULL, NULL, &tv) > 0) {
-        if (read(pipefd[i * 2], &min, sizeof(int)) == sizeof(int) &&
-            read(pipefd[i * 2], &max, sizeof(int)) == sizeof(int)) {
-          result_available = true;
+        if (FD_ISSET(pipefd[i * 2], &readfds)) {
+          read(pipefd[i * 2], &min, sizeof(int));
+          read(pipefd[i * 2], &max, sizeof(int));
+          result_available = 1;
         }
       }
       close(pipefd[i * 2]);
     }
 
     if (result_available) {
+      results_received++;
       if (min < min_max.min) min_max.min = min;
       if (max > min_max.max) min_max.max = max;
-    } else {
-      printf("No result from child process %d (may have been terminated)\n", child_pids[i]);
     }
   }
 
@@ -293,10 +303,17 @@ int main(int argc, char **argv) {
   elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
 
   free(array);
+  free(child_pids);
 
   printf("Min: %d\n", min_max.min);
   printf("Max: %d\n", min_max.max);
+  printf("Results received from %d out of %d processes\n", results_received, pnum);
   printf("Elapsed time: %fms\n", elapsed_time);
+  
+  if (timeout_occurred) {
+    printf("WARNING: Execution terminated due to timeout!\n");
+  }
+  
   fflush(NULL);
   return 0;
 }
